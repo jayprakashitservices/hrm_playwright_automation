19ba69b597ac4e787b5d88cc0d86805b11543f46
jayprakashitservices <jayprakash@itservicesindia.com>
2026-04-23 18:57:44 +0530

    Fix test path

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     baseURL: process.env.BASE_URL,
-    headless: false,
+    headless: true,
 
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
